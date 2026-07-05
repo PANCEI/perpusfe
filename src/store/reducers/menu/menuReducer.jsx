c0a74e6ca8e984fa633174209656fa12ae6097bb
PANCE <aswarhamid12@gmail.com>
2026-07-05 14:02:21 +0700

    sa

--- src/store/reducers/menu/menuReducer.jsx
+++ src/store/reducers/menu/menuReducer.jsx
@@ -1,21 +1,21 @@
 const initialState = {
-    menus :[],
-    isLoading:false,
-    error:null
+    menus: [],
+    isLoading: false,
+    error: null
 }
-export default function menuReducer(state= initialState , action ){
-switch (action.type){
-    case 'GET_MENU_START':
-    case 'SET_MENU_LOADING':
-        return {...state , isLoading:true , error:null};
-    case 'SET_DYNAMIC_MENUS':
-        return {...state , menus:action.payload, isLoading:false , error :null};
-    case 'GET_MENU_FAIL':
-        return {...state , isLoading:false , error:action.payload};
-    case 'AUTH_LOG_OUT':
-        return initialState;
-    default:
-        return state;
-    
-}
+export default function menuReducer(state = initialState, action) {
+    switch (action.type) {
+        case 'GET_MENU_START':
+        case 'SET_MENU_LOADING':
+            return { ...state, isLoading: true, error: null };
+        case 'SET_DYNAMIC_MENUS':
+            return { ...state, menus: action.payload, isLoading: false, error: null };
+        case 'GET_MENU_FAIL':
+            return { ...state, isLoading: false, error: action.payload };
+        case 'AUTH_LOG_OUT':
+            return initialState;
+        default:
+            return state;
+
+    }
 }
