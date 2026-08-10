eca456d8da915416a35466f3c6c4b75b54468f1d
PANCE <aswarhamid12@gmail.com>
2026-08-10 23:03:13 +0800

    perbaikan get data master menu

--- app/Http/Controllers/Menu/MenuList.php
+++ app/Http/Controllers/Menu/MenuList.php
@@ -4,11 +4,19 @@ namespace App\Http\Controllers\Menu;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-
+use App\Models\Menu;
 class MenuList extends Controller
 {
     //
     public function getMenuList(){
-
+    // ambil semua menu utama
+    $menlist = Menu::without('submenus')->get();
+    return response()->json([
+            'status'  => 'success',
+            'message' => 'Berhasil mengambil daftar menu',
+            'data'    => [
+                'menuList' => $menlist
+            ]
+        ], 200);
     }
 }
